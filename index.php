d5d2eaead4fe0bb2e4404ac72263c08012c71118
kiemthe2009 <kiemtheghost@gmail.com>
2025-01-14 15:37:37 +0700

    new

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
-<?php
-echo "Hello, Heroku!";
-?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Frontend</title>
+</head>
+<body>
+    <h1>Welcome to the frontend!</h1>
+    <p>Backend is running on Heroku: <a href="https://kiemthe2009.herokuapp.com">Go to backend</a></p>
+</body>
+</html>
