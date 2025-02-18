93e035f3962ef6bf84e54dd3d3b202a885087356
kiemthe2009 <kiemtheghost@gmail.com>
2025-02-18 17:04:42 +0700

    newsite
    d

---
+++ myjs.js
@@ -0,0 +1,16 @@
+﻿function printw() {
+    $("#imgprint").hide();
+    window.print();
+
+}
+window.onafterprint = function () {
+    $("#imgprint").show(); window.close();
+}
+$(document).ready(function () {
+    printw();
+})
+jQuery(document).bind("keyup keydown", function (e) {
+    if (e.ctrlKey && e.keyCode == 80) {
+        $("#imgprint").hide();
+    }
+});
